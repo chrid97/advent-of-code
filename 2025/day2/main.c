2485185c9491bdc5e2d26e20e605a4317ab5a1c5
Christopher Goncalves <cgoncal12@gmail.com>
2025-12-07 21:19:24 -0800

    Redo dir structure and move all code to one 2025main.c

--- 2025/day2/main.c
+++
@@ -1,9 +0,0 @@
-#include "../util.c"
-#include <stdio.h>
-
-int main(void) {
-  ReadFileResult file = file_read("./day2.txt");
-  for (int i = 0; i < file.size; i++) {
-    printf("%c", file.buffer[i]);
-  }
-}
